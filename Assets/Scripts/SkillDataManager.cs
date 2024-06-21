b34d14778a10a078bfb12c8fcc3a3dc6d27f0f5d
KimHyunTae <rarncron@gmail.com>
2024-06-21 18:00:30 +0900

    데이터 매니저 작업

--- Assets/Scripts/SkillDataManager.cs
+++ Assets/Scripts/SkillDataManager.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Xml.Linq;
 using UnityEngine;
 
 public class SkillDataManager : MonoBehaviour
@@ -19,6 +19,45 @@ public class SkillDataManager : MonoBehaviour
 
     }
 
+    private void ReadAttackSkillTable(string tableName)
+    {
+        LoadedAttackSkillList = new Dictionary<string, AttackSkill>();
 
+        XDocument doc = XDocument.Load($"{_dataRootPath}/{tableName}.xml");
+        var dataElements = doc.Descendants("data");
+
+        foreach ( var data in dataElements )
+        {
+            var tempAttack = new AttackSkill();
+            tempAttack.SkillName = data.Attribute("DataName").Value;
+            tempAttack.Name = data.Attribute(nameof(tempAttack.Name)).Value;
+            tempAttack.Description = data.Attribute(nameof(tempAttack.Description)).Value;
+            tempAttack.BaseDamaged = float.Parse(data.Attribute(nameof(tempAttack.BaseDamaged)).Value);
+            tempAttack.DamageMultiSkillLevelName =float.Parse(data.Attribute(nameof(tempAttack.DamageMultiSkillLevelName)).Value);
+
+            string attackSkillNameListStr = data.Attribute(nameof(tempAttack.AttackSkillNameList)).Value;
+            if(!string.IsNullOrEmpty(attackSkillNameListStr) )
+            {
+                attackSkillNameListStr = attackSkillNameListStr.Replace("{", string.Empty);
+                attackSkillNameListStr = attackSkillNameListStr.Replace("}", string.Empty);
+
+                var names = attackSkillNameListStr.Split(',');
+
+                var list = new List<string>();
+                if(names.Length > 0)
+                {
+                    foreach( var name in names )
+                    {
+                        list.Add(name);
+                    }
+                    tempAttack.AttackSkillNameList = list;
+                }
+                LoadedAttackSkillList.Add(tempAttack.SkillName, tempAttack);
+            }
+
+        }
+
+        
+    }
 
 }
