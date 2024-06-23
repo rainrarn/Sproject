3e27d803c6bff8b93357dabf55668ef935524645
Kimhyuntae <tie314@naver.com>
2024-06-23 17:10:37 +0900

    스킬트리 UI

---
+++ Assets/Scripts/SkilltreeUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillTreeUI : MonoBehaviour
+{
+    public SkillTree skillTree;
+    public GameObject skillButtonPrefab;
+    public GameObject incrementButtonPrefab;
+    public GameObject decrementButtonPrefab;
+    public Transform skillButtonParent;
+
+    
+}
+
