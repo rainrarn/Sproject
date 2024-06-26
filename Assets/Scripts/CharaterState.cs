1a636a89838da721d14b70375df7e9dd8a16e75a
KimHyunTae <rarncron@gmail.com>
2024-06-26 14:55:15 +0900

    IdleState와 AtkState추가

--- Assets/Scripts/CharaterState.cs
+++ Assets/Scripts/CharaterState.cs
@@ -26,5 +26,62 @@ public class IdleState : StateBase
         _player = player;
     }
 
+    public override void EnterState()
+    {
+        _player.BindInputCallback(true, OnInputCallback);
+    }
+
+    public override void ExitState()
+    {
+        _player.BindInputCallback(false, OnInputCallback);
+    }
+
+    public override void OnInputCallback(InputAction.CallbackContext context)
+    {
+        if(context.action.name == "Atk")
+        {
+            _player.ChangeState(new AtkState(_player));
+        }
+        if(context.action.name == "Move")
+        {
+            _player.ChangeState(new MoveState(_player));
+        }
+    }
+}
+
+public class MoveState : StateBase
+{
+    private readonly PlayerView _player;
+    public MoveState(PlayerView player)
+    {
+        _player = player;
+    }
+
+    public override void EnterState()
+    {
+        _player.Animator_Player.SetTrigger("Move");
+    }
     
+}
+public class AtkState : StateBase
+{
+    private readonly PlayerView _player;
+    public AtkState(PlayerView player)
+    {
+        _player = player;
+    }
+
+    public override void EnterState()
+    {
+        _player.Animator_Player.SetTrigger("Atk");
+    }
+
+    public override void ExecuteOnUpdate()
+    {
+        var animInfo = _player.Animator_Player.GetCurrentAnimatorStateInfo(0);
+        if(animInfo.normalizedTime>1)
+        {
+            _player.ChangeState(new IdleState(_player));
+        }
+    }
 }
--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -1,11 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerView : MonoBehaviour
 {
     [SerializeField] TextMesh TextMesh_Name;
 
     public TextMesh TextMesh_Level;
     public Animator Animator_Player;
+
+    private IState _curState;
+
+    private Action<InputAction.CallbackContext> _inputCallback;
+
+    private void Start()
+    {
+        ChangeState(new IdleState(this));
+    }
+
+    public void ChangeState(IState newState)
+    {
+        _curState?.ExitState();
+        _curState = newState;
+        _curState.EnterState();
+    }
+
+    public void BindInputCallback(bool isBind, Action<InputAction.CallbackContext> callback)
+    {
+        if(isBind)
+            _inputCallback += callback;
+        else
+            _inputCallback -= callback;
+    }
+
+    public void OnActionInput(InputAction.CallbackContext context)
+    {
+        _inputCallback?.Invoke(context);
+    }
 }
