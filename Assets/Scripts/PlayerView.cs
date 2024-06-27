d55fc9b16444bcb7f9117b92712b83a7ae3e85c4
KimHyunTae <rarncron@gmail.com>
2024-06-27 18:07:26 +0900

    플레이어 이동 스크립트

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,54 +6,67 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerView : MonoBehaviour
 {
-    [SerializeField] TextMesh TextMesh_Name;
+    [Header("Camera")]
+    public GameObject CinemachineCameraTarget;
 
-    public TextMesh TextMesh_Level;
-    public Animator Animator_Player;
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float TopClamp = 70.0f;
 
-    private IState _curState;
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float BottomClamp = -30.0f;
 
-    private Action<InputAction.CallbackContext> _inputCallback;
+    [Tooltip("Additional degrees to override the camera. Useful for fine-tuning camera position when locked")]
+    public float CameraAngleOverride = 0.0f;
 
-    private Vector2 _moveInput;
+    [Tooltip("For locking the camera position on all axis")]
+    public bool LockCameraPosition = false;
+
+    // cinemachine
+    private float _cinemachineTargetYaw;
+    private float _cinemachineTargetPitch;
+
+    private PlayerInputs _input;
+    private GameObject _mainCamera;
+
+    private const float _threshold = 0.01f;
+
+    private void Awake()
+    {
+        if (_mainCamera == null)
+        {
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
 
     private void Start()
     {
-        ChangeState(new IdleState(this));
+        _input = GetComponent<PlayerInputs>();
     }
 
-    public void ChangeState(IState newState)
+    private void LateUpdate()
     {
-        _curState?.ExitState();
-        _curState = newState;
-        _curState.EnterState();
+        CameraRotation();
     }
 
-    public void BindInputCallback(bool isBind, Action<InputAction.CallbackContext> callback)
+    private void CameraRotation()
     {
-        if(isBind)
-            _inputCallback += callback;
-        else
-            _inputCallback -= callback;
+        if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+        {
+            _cinemachineTargetYaw += _input.look.x * Time.deltaTime;
+            _cinemachineTargetPitch += _input.look.y * Time.deltaTime;
+        }
+
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
     }
 
-    public void OnActionInput(InputAction.CallbackContext context)
+    private static float ClampAngle(float angle, float min, float max)
     {
-        _inputCallback?.Invoke(context);
-    }
-
-    public Vector2 GetMoveInput()
-    {
-        return _moveInput;
-    }
-
-    public void OnMoveInput(InputAction.CallbackContext context)
-    {
-        _moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void Move(Vector2 moveInput)
-    {
-        
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
     }
 }
+
